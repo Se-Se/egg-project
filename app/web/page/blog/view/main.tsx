32690f3cf7d0d63b294c78bd1dcdebd189deba7f
v_cbbochen <v_cbbochen@tencent.com>
2022-10-10 17:01:16 +0800

    fix less-loader

--- app/web/page/blog/view/main.tsx
+++ app/web/page/blog/view/main.tsx
@@ -27,7 +27,7 @@ class Main extends Component {
     return <Layout {...this.props}>
       <Header></Header>
       <ul className="menu-tab">
-        <li onClick={this.tabClick.bind(this)}><Link to="/">Home1</Link></li>
+        <li onClick={this.tabClick.bind(this)}><Link to="/">Home111</Link></li>
         <li onClick={this.tabClick.bind(this)}><Link to="/async">Async</Link></li>
         <li onClick={this.tabClick.bind(this)}><Link to="/example">Example</Link></li>
         <li onClick={this.tabClick.bind(this)}><Link to="/about">About</Link></li>
